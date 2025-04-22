a6e408a9ec31a9983d40861ca5e932392e3ff7fe
AteCookie <k024g1012@g.neec.ac.jp>
2025-04-22 11:58:37 +0900

    1

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@ const char kWindowTitle[] = "GC2C_03_キョク_キンウ";
 const int kWindowWidth = 1280; // 画面の横幅
 const int kWindowHeight = 720; // 画面の縦幅
 
+struct Matrix4x4
+{
+	float m[4][4];
+};
+
+//平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+//座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+
+
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -31,6 +45,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 translate = { 4.1f, 2.6f, 0.8f };
+		Vector3 scale = { 1.5f,5.2f, 7.3f };
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Vector3 point{ 2.3f,3.8f,1.4f };
+
+		Matrix4x4 transformMatrix = {
+			1.0f,2.0f,3.0f,4.0f,
+			3.0f,1.0f,1.0f,2.0f,
+			1.0f,4.0f,2.0f,3.0f,
+			2.0f,2.0f,1.0f,3.0f
+		};
+
+		Vector3 transformedPoint = Transform(point, transformMatrix);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -40,6 +69,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		
 
 		///
 		/// ↑描画処理ここまで
