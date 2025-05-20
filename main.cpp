63f30764ed00dbe22f199541dc61b57051e253cd
AteCookie <k024g1012@g.neec.ac.jp>
2025-05-20 10:40:36 +0900

    123

--- main.cpp
+++ main.cpp
@@ -11,130 +11,84 @@ struct Matrix4x4
 	float m[4][4];
 };
 
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
-{
-	Matrix4x4 result;
-	result.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
-	result.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
-	result.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
-	result.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3];
+Matrix4x4 MakePerspectiverFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4  MakePerspectiverFovMatrix;
+	MakePerspectiverFovMatrix.m[0][0] = 1 / aspectRatio * (1 / std::tan(fovY / 2));
+	MakePerspectiverFovMatrix.m[0][1] = 0.0f;
+	MakePerspectiverFovMatrix.m[0][2] = 0.0f;
+	MakePerspectiverFovMatrix.m[0][3] = 0.0f;
 
-	result.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0];
-	result.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1];
-	result.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2];
-	result.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3];
+	MakePerspectiverFovMatrix.m[1][0] = 0.0f;
+	MakePerspectiverFovMatrix.m[1][1] = 1 / std::tan(fovY / 2);
+	MakePerspectiverFovMatrix.m[1][2] = 0.0f;
+	MakePerspectiverFovMatrix.m[1][3] = 0.0f;
 
-	result.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0];
-	result.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1];
-	result.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2];
-	result.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
+	MakePerspectiverFovMatrix.m[2][0] = 0.0f;
+	MakePerspectiverFovMatrix.m[2][1] = 0.0f;
+	MakePerspectiverFovMatrix.m[2][2] = farClip / (farClip - nearClip);
+	MakePerspectiverFovMatrix.m[2][3] = 1.0f;
 
-	result.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] + m1.m[3][3] * m2.m[3][0];
-	result.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1];
-	result.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2];
-	result.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
-	return result;
+	MakePerspectiverFovMatrix.m[3][0] = 0.0f;
+	MakePerspectiverFovMatrix.m[3][1] = 0.0f;
+	MakePerspectiverFovMatrix.m[3][2] = -nearClip * farClip / (farClip - nearClip);
+	MakePerspectiverFovMatrix.m[3][3] = 0.0f;
+
+	return MakePerspectiverFovMatrix;
 }
 
-Matrix4x4 MakeRotateXMatrix(float radian) {
-	Matrix4x4 result;
-	result.m[0][0] = 1;
-	result.m[0][1] = 0;
-	result.m[0][2] = 0;
-	result.m[0][3] = 0;
+//正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
 
-	result.m[1][0] = 0;
-	result.m[1][1] = std::cos(radian);
-	result.m[1][2] = std::sin(radian);
-	result.m[1][3] = 0;
+	/*assert(left != right);
+	assert(top != bottom);*/
+	Matrix4x4 MakeOrthographicMatrix;
+	MakeOrthographicMatrix.m[0][0] = 2.0f / (right - left);
+	MakeOrthographicMatrix.m[0][1] = 0.0f;
+	MakeOrthographicMatrix.m[0][2] = 0.0f;
+	MakeOrthographicMatrix.m[0][3] = 0.0f;
 
-	result.m[2][0] = 0;
-	result.m[2][1] = -std::sin(radian);
-	result.m[2][2] = std::cos(radian);
-	result.m[2][3] = 0;
+	MakeOrthographicMatrix.m[1][0] = 0.0f;
+	MakeOrthographicMatrix.m[1][1] = 2.0f / (top - bottom);
+	MakeOrthographicMatrix.m[1][2] = 0.0f;
+	MakeOrthographicMatrix.m[1][3] = 0.0f;
 
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
-	return result;
+	MakeOrthographicMatrix.m[2][0] = 0.0f;
+	MakeOrthographicMatrix.m[2][1] = 0.0f;
+	MakeOrthographicMatrix.m[2][2] = 1 / (farClip - nearClip);
+	MakeOrthographicMatrix.m[2][3] = 0.0f;
+
+	MakeOrthographicMatrix.m[3][0] = (left + right) / (left - right);
+	MakeOrthographicMatrix.m[3][1] = (top + bottom) / (bottom - top);
+	MakeOrthographicMatrix.m[3][2] = nearClip / (nearClip - farClip);
+	MakeOrthographicMatrix.m[3][3] = 1.0f;
+
+	return MakeOrthographicMatrix;
+
 }
 
-Matrix4x4 MakeRotateYMatrix(float radian) {
-	Matrix4x4 result;
-	result.m[0][0] = std::cos(radian);
-	result.m[0][1] = 0;
-	result.m[0][2] = -std::sin(radian);
-	result.m[0][3] = 0;
+//ビューポート行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 MakeViewportMatrix;
+	MakeViewportMatrix.m[0][0] = width / 2.0f;
+	MakeViewportMatrix.m[0][1] = 0.0f;
+	MakeViewportMatrix.m[0][2] = 0.0f;
+	MakeViewportMatrix.m[0][3] = 0.0f;
 
-	result.m[1][0] = 0;
-	result.m[1][1] = 1;
-	result.m[1][2] = 0;
-	result.m[1][3] = 0;
+	MakeViewportMatrix.m[1][0] = 0.0f;
+	MakeViewportMatrix.m[1][1] = -height / 2.0f;
+	MakeViewportMatrix.m[1][2] = 0.0f;
+	MakeViewportMatrix.m[1][3] = 0.0f;
 
-	result.m[2][0] = std::sin(radian);
-	result.m[2][1] = 0;
-	result.m[2][2] = std::cos(radian);
-	result.m[2][3] = 0;
+	MakeViewportMatrix.m[2][0] = 0.0f;
+	MakeViewportMatrix.m[2][1] = 0.0f;
+	MakeViewportMatrix.m[2][2] = maxDepth - minDepth;
+	MakeViewportMatrix.m[2][3] = 0.0f;
+	MakeViewportMatrix.m[3][0] = left + (width / 2.0f);
 
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
-	return result;
-}
-
-Matrix4x4 MakeRotateZMatrix(float radian) {
-	Matrix4x4 result;
-	result.m[0][0] = std::cos(radian);
-	result.m[0][1] = std::sin(radian);
-	result.m[0][2] = 0;
-	result.m[0][3] = 0;
-
-	result.m[1][0] = -std::sin(radian);
-	result.m[1][1] = std::cos(radian);
-	result.m[1][2] = 0;
-	result.m[1][3] = 0;
-
-	result.m[2][0] = 0;
-	result.m[2][1] = 0;
-	result.m[2][2] = 1;
-	result.m[2][3] = 0;
-
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
-	return result;
-}
-
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-
-	Matrix4x4 rotateMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
-
-	Matrix4x4 result;
-
-	result.m[0][0] = scale.x * rotateMatrix.m[0][0];
-	result.m[0][1] = scale.x * rotateMatrix.m[0][1];
-	result.m[0][2] = scale.x * rotateMatrix.m[0][2];
-	result.m[0][3] = 0.0f;
-	result.m[1][0] = scale.y * rotateMatrix.m[1][0];
-	result.m[1][1] = scale.y * rotateMatrix.m[1][1];
-	result.m[1][2] = scale.y * rotateMatrix.m[1][2];
-	result.m[1][3] = 0.0f;
-	result.m[2][0] = scale.z * rotateMatrix.m[2][0];
-	result.m[2][1] = scale.z * rotateMatrix.m[2][1];
-	result.m[2][2] = scale.z * rotateMatrix.m[2][2];
-	result.m[2][3] = 0.0f;
-	result.m[3][0] = translate.x;
-	result.m[3][1] = translate.y;
-	result.m[3][2] = translate.z;
-	result.m[3][3] = 1.0f;
-
-	return result;
+	MakeViewportMatrix.m[3][1] = top + (height / 2.0f);
+	MakeViewportMatrix.m[3][2] = minDepth;
+	MakeViewportMatrix.m[3][3] = 1.0f;
+	return MakeViewportMatrix;
 }
 
 static const int kRowHeight = 20;
@@ -164,11 +118,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	/// 初期処理
 
-	Vector3 scale{ 1.2f,0.79f,-2.1f };
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };
-	Vector3 translate{ 2.7f,-4.15f,1.57f };
-	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
-
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -181,7 +130,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
+		Matrix4x4 orthographicMatrix =
+			MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+		Matrix4x4 perspectiveFovMatrix =
+			MakePerspectiverFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+		Matrix4x4 viewportMatrix =
+			MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 		
 
 		///
@@ -193,8 +147,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		Novice::ScreenPrintf(0, 0, "worldMatrix");
-		MatrixScreenPrintf(0, 20, worldMatrix, "worldMatrix");
+		Novice::ScreenPrintf(0, 0, "orthographicMatrix");
+		MatrixScreenPrintf(0, 20, orthographicMatrix, "orthographicMatrix");
+		Novice::ScreenPrintf(0, kRowHeight * 5, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 + kRowHeight, perspectiveFovMatrix, "perspectiveFovMatrix");
+		Novice::ScreenPrintf(0, kRowHeight * 10, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10 + kRowHeight, viewportMatrix, "viewportMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
