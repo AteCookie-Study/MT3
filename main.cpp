7cac78fd790df6869c37f038f09603f5716c431c
AteCookie <k024g1012@g.neec.ac.jp>
2025-06-24 10:21:24 +0900

    2-2

--- main.cpp
+++ main.cpp
@@ -31,7 +31,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraRotate = { 0.26f,0.0f,0.0f };
 
 	Sphere sphere1 = { 0.0f,0.0f, 0.0f, 1.0f };
-	Sphere sphere2 = { 0.5f,0.0f, 0.5f, 0.5f };
+
+	Plane plane = { 1.0f,1.0f,1.0f,1.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -51,6 +52,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 worldMViewProjectionMatrix = MultiplyMatrix(worldMatrix, MultiplyMatrix(viewMatrix, projectionMatrix));
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -61,7 +63,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		DrawGrid(worldMViewProjectionMatrix, viewportMatrix);
-		if (InCollision(sphere1, sphere2))
+		if (InCollision(sphere1, plane))
 		{
 			DrawSphere(sphere1, worldMViewProjectionMatrix, viewportMatrix, RED);
 		}
@@ -69,15 +71,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{
 			DrawSphere(sphere1, worldMViewProjectionMatrix, viewportMatrix, BLACK);
 		}
-		DrawSphere(sphere2, worldMViewProjectionMatrix, viewportMatrix, BLACK);
 		ImGui::Begin("Window");
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 		ImGui::DragFloat3("Sphere1Center", &sphere1.center.x, 0.01f);
 		ImGui::DragFloat("Sphere1Radius", &sphere1.radius, 0.01f);
-		ImGui::DragFloat3("Sphere2Center", &sphere2.center.x, 0.01f);
-		ImGui::DragFloat("Sphere2Radius", &sphere2.radius, 0.01f);
 		ImGui::End();
+
+		DrawPlane(plane, worldMViewProjectionMatrix, viewportMatrix, BLACK);
 
 		///
 		/// ↑描画処理ここまで
