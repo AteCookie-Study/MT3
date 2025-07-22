341bffa3c0ad07f1049d15778b6e8b6e27586e34
AteCookie <k024g1012@g.neec.ac.jp>
2025-07-22 10:15:25 +0900

    .

--- Cal.h
+++ Cal.h
@@ -50,27 +50,7 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 
 		}
 	}
-
-	/*result.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
-	result.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
-	result.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
-	result.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3];
-
-	result.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0];
-	result.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1];
-	result.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2];
-	result.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3];
-
-	result.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0];
-	result.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1];
-	result.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2];
-	result.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
-
-	result.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] + m1.m[3][3] * m2.m[3][0];
-	result.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1];
-	result.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2];
-	result.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];*/
-	return result;
+return result;
 };
 float Length(const Vector3& v) {
 	float result;
@@ -170,15 +150,10 @@ Matrix4x4 MakeRotateZMatrix(float radian) {
 Matrix4x4 MakeAffineMatrix(const Vector3 scale, const Vector3 rotate, const Vector3 translate) {
 	Matrix4x4 result{};
 
-	/*Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-
-	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);*/
 	Matrix4x4 rotateXYZMatrix = Multiply(
 		MakeRotateXMatrix(rotate.x),
 		Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
 
-	/*result2 = Multiply(rotateXYZMatrix, translateMatrix);
-	result = Multiply(rotateXYZMatrix, scaleMatrix);*/
 	result.m[0][0] = rotateXYZMatrix.m[0][0] * scale.x;
 	result.m[0][1] = rotateXYZMatrix.m[0][1] * scale.x;
 	result.m[0][2] = rotateXYZMatrix.m[0][2] * scale.x;
@@ -470,16 +445,6 @@ Matrix4x4 Transpose(const Matrix4x4& m) {
 			result.m[row][column] = m.m[column][row];
 		}
 	}
-
-	//result.m[0][0] = m.m[0][0];
-	//result.m[0][1] = m.m[1][0];
-	//result.m[0][2] = m.m[2][0];
-	//result.m[0][3] = m.m[3][0];
-
-	//result.m[1][0] = m.m[0][1];
-	//result.m[1][1] = m.m[1][1];
-	//result.m[1][2] = m.m[2][1];
-	//result.m[1][3] = m.m[3][1];
 
 
 	return result;
